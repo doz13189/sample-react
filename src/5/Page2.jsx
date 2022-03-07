a0fc24f8b6a918eb8eff98115d1396f2e34ac79c
takahiro kato <dozo13189@gmail.com>
2022-03-07 16:04:43 +0900

    5-19

---
+++ src/5/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+    return (
+        <div>
+            <h1>page 2</h1>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
+
 import React from 'react'
-import Four from './4/four'
+import { Home } from './5/Home';
+import { Page1 } from './5/Page1';
+import { Page2 } from './5/Page2';
 
 function App() {
 
   return (
     <div>
-      <Four />
+      <BrowserRouter>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/page1'>Page1</Link></li>
+          <li><Link to='/page2'>Page2</Link></li>
+        </ul>
+
+        <Switch>
+
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route exact path='/page1'>
+            <Page1 />
+          </Route>
+
+          <Route exact path='/page2'>
+            <Page2 />
+          </Route>
+
+        </Switch>
+
+      </BrowserRouter>
     </div>
   );
 }
