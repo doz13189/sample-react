2d32b6ed3dbb7127b9540fbfabec7db091abdba2
takahiro kato <dozo13189@gmail.com>
2022-03-08 23:12:53 +0900

    7-38

--- src/6/components/atoms/button/PrimaryButton.jsx
+++ src/6/components/atoms/button/PrimaryButton.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
 import { SBaseButton } from './BaseButton'
+import { useContext } from 'react'
+import { UserContext } from '../../../../7/prividers/UserProvider'
+
 
 export const PrimaryButton = (props) => {
     const { children } = props
+    const { setUserInfo } = useContext(UserContext)
+    const eventOnClick = () => { setUserInfo({ isAdmin: true }) }
+
     return (
-        <SButton>{ children }</SButton>
+        <SButton onClick={eventOnClick}>{ children }</SButton>
     )
 }
 
--- src/7/prividers/UserProvider.jsx
+++ src/7/prividers/UserProvider.jsx
@@ -1,12 +1,15 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 export const UserContext = createContext({})
 
 export const UserProvider = (props) => {
     const { children } = props
-    const contextName = 'tk'
+
+    const [ userInfo, setUserInfo ] = useState(null)
+    console.log(userInfo)
+
     return (
-        <UserContext.Provider value={{ contextName }}>
+        <UserContext.Provider value={{ userInfo, setUserInfo }}>
             { children }
         </UserContext.Provider>
     )
